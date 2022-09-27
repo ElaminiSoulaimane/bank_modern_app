c33a2bb4f0151b312551af20c55f0eb7c3d37618
Soulaimane Elamini <96657205+ElaminiSoulaimane@users.noreply.github.com>
2022-09-27 22:42:32 +0100

    Edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import {useState} from 'react'
+import { close , logo , menu} from '../assets';
+import {navLinks} from '../constants'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+        <img src={logo} alt="codingpalace" className='w-[124px] h-[32px]' />
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {
+          navLinks.map((nav,index)=>(
+            <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
+
+            </li>
+          ))
+        }
+
+        </ul>
+    </nav>
   )
 }
 
